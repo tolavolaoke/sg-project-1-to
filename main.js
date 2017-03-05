75d7c9f41c34faf0c4036b899972d0c2c935856c
Tola Olaoke <tolaoke@spartaglobal.co>
2017-03-05 15:17:18 +0000

    adjusted canvas and drew the rectangle block onto canvas

--- main.js
+++ main.js
@@ -1,5 +1,12 @@
 console.log($);
 console.log('Java Script Works!');
 
-var canvas = getElementById('#canvas');
-var ctx = canvas.getContext('2d');  ctx.fillRect(10, 10, 100, 100);
+//---------CANVAS AND DRAWING OBJECT---------------------
+var canvas = document.getElementById('canvas');//referencing the canvas element to the canvas variable
+var ctx = canvas.getContext('2d'); //varaible to store the 2D shape (rectangle)
+// The rectangle shape (the main colour changing block)
+ctx.beginPath();
+ctx.rect(200, 570, 100, 30);//(x- axis, y- axis, width, height of the rectangle block)
+ctx.fillStyle = 'black';
+ctx.fill();
+ctx.closePath();
