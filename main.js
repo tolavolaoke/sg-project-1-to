708a2690b24590d4960da014a2e0ff3aa49da79c
Tola Olaoke <tolaoke@spartaglobal.co>
2017-03-09 15:51:22 +0000

    added fade out to the blocks when they hit the rectangle

--- main.js
+++ main.js
@@ -20,16 +20,44 @@ function addRandomBlock() {
 
 //FUNCTION THAT MAKES THE BLOCKS FALL/DROPDOWN WHEN START BUTTON IS CLICKED
 $('.startBtn').click(function(){
+
+  // var intervalID = window.setInterval(checkCollisions, 100);
+
   var numberOfBlocks = 20; // total number of blocks in the loop
   var $randomBlocks = [];
   var intervalBetweenBlockDrops = 1000; // milliseconds
+
 
   for (var i = 1 ; i <= numberOfBlocks ; i++) {
     $randomBlocks.push(addRandomBlock());
   }
   $randomBlocks.forEach(function ($randomBlock, i) {
     setTimeout(function () {
+
+
       $randomBlock.addClass('drop-down');
+      var intervalID = window.setInterval(function () {
+        var hasCollided = checkCollision($randomBlock.offset().top);
+        if (hasCollided) {
+          window.clearInterval(intervalID);
+          collisionDetected($randomBlock);
+        }
+      }, 100);
+
+
     }, intervalBetweenBlockDrops * (i+1));
   });
 });
+
+function collisionDetected ($el) {
+  console.log('we have a collision!');
+  $el.fadeOut();
+}
+function checkCollision (randomOffsetTop) {
+  console.log('checking offset....');
+  // console.log(baseOffset)
+  // console.log(randomOffsetTop)
+  // console.log()
+  return randomOffsetTop === 608;
+
+}
